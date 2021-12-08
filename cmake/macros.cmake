79cb1904de7b67267d63f80d43213ced6d0a2a22
David Dight <davidd@fix8mt.com>
2021-12-08 13:26:34 +1100

    licensce, source update

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -1,3 +1,46 @@
+###########################################################################################
+##
+##    ____                      __      ____
+##   /\  _`\   __             /'_ `\   /\  _`\
+##   \ \ \L\_\/\_\    __  _  /\ \L\ \  \ \ \L\ \ _ __    ___
+##    \ \  _\/\/\ \  /\ \/'\ \/_> _ <_  \ \ ,__//\`'__\ / __`\
+##     \ \ \/  \ \ \ \/>  </   /\ \L\ \  \ \ \/ \ \ \/ /\ \L\ \
+##      \ \_\   \ \_\ /\_/\_\  \ \____/   \ \_\  \ \_\ \ \____/
+##       \/_/    \/_/ \//\/_/   \/___/     \/_/   \/_/  \/___/
+##
+##               Fix8Pro FIX Engine and Framework
+##
+## Copyright (C) 2010-21 Fix8 Market Technologies Pty Ltd (ABN 29 167 027 198)
+## All Rights Reserved. [http://www.fix8mt.com] <heretohelp@fix8mt.com>
+##
+## This  file is released  under the  GNU LESSER  GENERAL PUBLIC  LICENSE  Version 2.  You can
+## redistribute  it  and / or modify  it under the  terms of  the  GNU Lesser  General  Public
+## License as  published  by  the Free  Software Foundation,  either version 2 of the License,
+## or (at your option) any later version.
+##
+## This file is distributed in the hope that it will be useful, but  WITHOUT ANY WARRANTY  and
+## without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+##
+## You should  have received a copy of  the GNU Lesser General Public  License along with this
+## file. If not, see <http://www.gnu.org/licenses/>.
+##
+## BECAUSE THE PROGRAM IS  LICENSED FREE OF  CHARGE, THERE IS NO  WARRANTY FOR THE PROGRAM, TO
+## THE EXTENT  PERMITTED  BY  APPLICABLE  LAW.  EXCEPT WHEN  OTHERWISE  STATED IN  WRITING THE
+## COPYRIGHT HOLDERS AND/OR OTHER PARTIES  PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY OF ANY
+## KIND,  EITHER EXPRESSED   OR   IMPLIED,  INCLUDING,  BUT   NOT  LIMITED   TO,  THE  IMPLIED
+## WARRANTIES  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  THE ENTIRE RISK AS TO
+## THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE,
+## YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+##
+## IN NO EVENT UNLESS REQUIRED  BY APPLICABLE LAW  OR AGREED TO IN  WRITING WILL ANY COPYRIGHT
+## HOLDER, OR  ANY OTHER PARTY  WHO MAY MODIFY  AND/OR REDISTRIBUTE  THE PROGRAM AS  PERMITTED
+## ABOVE,  BE  LIABLE  TO  YOU  FOR  DAMAGES,  INCLUDING  ANY  GENERAL, SPECIAL, INCIDENTAL OR
+## CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT
+## NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR
+## THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS), EVEN IF SUCH
+## HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+##
+#############################################################################################
 macro(add_gen_library shared name xml extra_fields)
 	if ("${extra_fields}" STREQUAL "_")
 		set(args ${ARGV4} ${ARGV5} ${ARGV6} ${ARGV7} ${ARGV8} ${ARGV9} ${ARGV10} ${ARGV11} ${ARGV12} ${ARGV13} ${ARGV14} ${CMAKE_SOURCE_DIR}/${xml})
--- simpleclisrv.cpp
+++ simpleclisrv.cpp
@@ -1,6 +1,5 @@
 //-----------------------------------------------------------------------------------------
 /*
-$Id: 598b59a1ffc24e51b39c33e760bbde649df502bc $
     ____                      __      ____
    /\  _`\   __             /'_ `\   /\  _`\
    \ \ \L\_\/\_\    __  _  /\ \L\ \  \ \ \L\ \ _ __    ___
@@ -14,13 +13,34 @@ $Id: 598b59a1ffc24e51b39c33e760bbde649df502bc $
 Copyright (C) 2010-21 Fix8 Market Technologies Pty Ltd (ABN 29 167 027 198)
 All Rights Reserved. [http://www.fix8mt.com] <heretohelp@fix8mt.com>
 
-THIS FILE IS PROPRIETARY AND  CONFIDENTIAL. NO PART OF THIS FILE MAY BE REPRODUCED,  STORED
-IN A RETRIEVAL SYSTEM,  OR TRANSMITTED, IN ANY FORM OR ANY MEANS,  ELECTRONIC, PHOTOSTATIC,
-RECORDED OR OTHERWISE, WITHOUT THE PRIOR AND  EXPRESS WRITTEN  PERMISSION  OF  FIX8  MARKET
-TECHNOLOGIES PTY LTD.
+This  file is released  under the  GNU LESSER  GENERAL PUBLIC  LICENSE  Version 2.  You can
+redistribute  it  and / or modify  it under the  terms of  the  GNU Lesser  General  Public
+License as  published  by  the Free  Software Foundation,  either version 2 of the License,
+or (at your option) any later version.
+
+This file is distributed in the hope that it will be useful, but  WITHOUT ANY WARRANTY  and
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+You should  have received a copy of  the GNU Lesser General Public  License along with this
+file. If not, see <http://www.gnu.org/licenses/>.
+
+BECAUSE THE PROGRAM IS  LICENSED FREE OF  CHARGE, THERE IS NO  WARRANTY FOR THE PROGRAM, TO
+THE EXTENT  PERMITTED  BY  APPLICABLE  LAW.  EXCEPT WHEN  OTHERWISE  STATED IN  WRITING THE
+COPYRIGHT HOLDERS AND/OR OTHER PARTIES  PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY OF ANY
+KIND,  EITHER EXPRESSED   OR   IMPLIED,  INCLUDING,  BUT   NOT  LIMITED   TO,  THE  IMPLIED
+WARRANTIES  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  THE ENTIRE RISK AS TO
+THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU. SHOULD THE PROGRAM PROVE DEFECTIVE,
+YOU ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED  BY APPLICABLE LAW  OR AGREED TO IN  WRITING WILL ANY COPYRIGHT
+HOLDER, OR  ANY OTHER PARTY  WHO MAY MODIFY  AND/OR REDISTRIBUTE  THE PROGRAM AS  PERMITTED
+ABOVE,  BE  LIABLE  TO  YOU  FOR  DAMAGES,  INCLUDING  ANY  GENERAL, SPECIAL, INCIDENTAL OR
+CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT
+NOT LIMITED TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR
+THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER PROGRAMS), EVEN IF SUCH
+HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
 
 */
-
 //-----------------------------------------------------------------------------------------
 #include <iostream>
 #include <memory>
